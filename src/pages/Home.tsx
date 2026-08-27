02356a6379832588fdd764edc95478ff3ae0d23b
unknown <jagonosalexandra@gmail.com>
2026-08-27 12:13:06 +0800

    Add fetaured recipes section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,32 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import hero from "../assets/hero.png";
+import { useSearch } from "../context/SearchContext";
 import Button from "../components/Button";
+import SearchBar from "../components/SearchBar";
+import { useNavigate } from "react-router-dom";
+import { useRandom } from "../hooks/useRecipes";
+import LoadingSpinner from "../components/LoadingSpinner";
+import RecipeCard from "../components/RecipeCard";
 
 function Home() {
+  const { search } = useSearch();
+  const { randomRecipes, fetchRandomRecipes, isLoading, error } = useRandom();
+  const navigate = useNavigate();
+
+  function handleSearch(query: string) {
+    search(query);
+    navigate("/search");
+  }
+
   const recipesRef = useRef<HTMLDivElement>(null);
 
   function scrollToRecipes() {
     recipesRef.current?.scrollIntoView({ behavior: "smooth" });
   }
+
+  useEffect(() => {
+    fetchRandomRecipes(6);
+  }, []);
 
   return (
     <div>
@@ -18,9 +37,9 @@ function Home() {
           alt=""
         />
         <div className="absolute inset-y-0 left-24 mt-auto px-8 py-12 max-w-md h-min flex flex-col justify-start items-start gap-3.5 text-white bg-green/80">
-          <p className="heading">
+          <h1 className="heading">
             Cook <span className="text-yellow">Smarter</span>, <br /> Not Harder
-          </p>
+          </h1>
           <p className="font-body mb-4">
             FindMyRecipe gives you total control. Narrow down options by
             cuisine, diet, and meal type, or shake things up with a surprise
@@ -36,7 +55,26 @@ function Home() {
         </div>
       </div>
 
-      <div ref={recipesRef}>Recipes</div>
+      <div className="section" ref={recipesRef}>
+        <h2>Browse through thousands of recipes</h2>
+
+        <SearchBar search={handleSearch} />
+
+        <div className="flex flex-wrap justify-between items-center gap-8 mt-16">
+          {isLoading && <LoadingSpinner />}
+          {error && <p className="error-text">{error}</p>}
+          {!isLoading &&
+            randomRecipes.map((recipe) => (
+              <RecipeCard
+                key={recipe.id}
+                id={recipe.id}
+                thumbnail={recipe.thumbnail}
+                name={recipe.name}
+                category={recipe.category}
+              />
+            ))}
+        </div>
+      </div>
 
       <div>Random</div>
     </div>
