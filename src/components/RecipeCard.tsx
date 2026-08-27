30781b4c9bb346b2e432503651dd619e9b286bef
unknown <jagonosalexandra@gmail.com>
2026-08-27 12:02:36 +0800

    Add RecipeCard

---
+++ src/components/RecipeCard.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+function RecipeCard({
+  id,
+  thumbnail,
+  name,
+  category,
+}: {
+  id: string;
+  thumbnail: string;
+  name: string;
+  category: string;
+}) {
+  return (
+    <Link
+      to={`/recipe/${id}`}
+      className="flex flex-col border-4 border-red-orange min-h-96 hover:border-green transition-colors duration-300"
+    >
+      <img
+        className="h-64 object-cover object-center"
+        src={thumbnail}
+        alt={name}
+      />
+      <p className="flex flex-col p-6">
+        <span className="text-gray-400">{category}</span>
+        <span className="text-2xl font-bold">{name}</span>
+      </p>
+    </Link>
+  );
+}
+
+export default RecipeCard;
