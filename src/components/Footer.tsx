7a3abe690e3900fc8f6907aec8dc13619818312e
unknown <jagonosalexandra@gmail.com>
2026-08-30 10:16:32 +0800

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="w-full px-24 py-6 bottom-0 bg-red-orange border-t-3 border-t-dark-red">
+      <p className="text-dark-red text-center text-sm">
+        &#xA9; FindMyRecipe by lexiethedeveloper. All rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
