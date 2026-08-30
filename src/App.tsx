23a6eb55a3174d96f34885f0dc15f6764414d57f
unknown <jagonosalexandra@gmail.com>
2026-08-30 11:07:29 +0800

    Add page navbar, footer, routing, and context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import { SearchContextProvider } from "./context/SearchContext";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import RecipeDetail from "./pages/RecipeDetail";
+import ScrollToTop from "./components/ScrollToTop";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <></>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <ScrollToTop />
+      <Navbar />
+      <main className="grow">
+        <SearchContextProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/recipe/:id" element={<RecipeDetail />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </SearchContextProvider>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
